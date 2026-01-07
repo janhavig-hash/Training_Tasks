f801583df630fc8e7064c75190148a826a943fe6
janhavig-hash <janhavi.g@glideclouds.com>
2026-01-07 17:47:50 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-def student_helper(student) -> dict:
-    return {
-        "id": str(student["_id"]),
-        "roll_no": student.get("roll_no"),
-        "name": student.get("name"),
-        "age": student.get("age"),
-        "department": student.get("department"),
-        "cgpa":student.get("cgpa"),
-    }
